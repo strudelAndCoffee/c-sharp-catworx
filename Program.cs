332cc5aa3c4d79870ce4d3e851a4406a75df04db
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-08-29 17:41:15 -0500

    update methods to handle full Employee class

--- Program.cs
+++ Program.cs
@@ -5,34 +5,41 @@ namespace CatWorx.BadgeMaker
 {
   class Program
   {
-    static List<string> GetEmployees()
+    static List<Employee> GetEmployees()
     {
-        List<string> employees = new List<string>();
+        List<Employee> employees = new List<Employee>();
         while (true)
         {
             Console.WriteLine("Please enter a name: (leave empty to exit): ");
             // null coalescing operator ??: if input is null, replace with empty string ""
-            string input = Console.ReadLine() ?? "";
-            if (input == "")
+            string firstName = Console.ReadLine() ?? "";
+            if (firstName == "")
             {
                 break;
             }
+            Console.Write("Enter last name: ");
+            string lastName = Console.ReadLine() ?? "";
+            Console.Write("Enter ID: ");
+            int id = Int32.Parse(Console.ReadLine() ?? "");
+            Console.Write("Enter Photo URL:");
+            string photoUrl = Console.ReadLine() ?? "";
 
-            Employee currentEmployee = new Employee(input, "Smith");
-            employees.Add(currentEmployee.GetFullName());
+            Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+            employees.Add(currentEmployee);
         }
         return employees;
     }
-    static void PrintEmplayees(List<string> employees)
+    static void PrintEmplayees(List<Employee> employees)
     {
-        for (int i = 0; i < employees.Count; i++)
+        for (int i = 0; i < employees.Count; i++) 
         {
-            Console.WriteLine(employees[i]);
+            string template = "{0,-10}\t{1,-20}\t{2}";
+            Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
         }
     }
     static void Main(string[] args)
     {
-        List<string> employees = GetEmployees();
+        List<Employee> employees = GetEmployees();
         PrintEmplayees(employees);
     }
   }
