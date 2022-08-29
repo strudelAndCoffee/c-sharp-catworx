3add9fa886ac10946f2b932d3e6521924100dace
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-08-29 16:58:05 -0500

    add methods for get/pring employees

--- Program.cs
+++ Program.cs
@@ -5,16 +5,33 @@ namespace CatWorx.BadgeMaker
 {
   class Program
   {
-    static void Main(string[] args)
+    static List<string> GetEmployees()
     {
-        List<string> employees = new List<string>() { "adam", "amy" };
-        employees.Add("barbara");
-        employees.Add("billy");
-
+        List<string> employees = new List<string>();
+        while (true)
+        {
+            Console.WriteLine("Please enter a name: (leave empty to exit): ");
+            // null coalescing operator ??: if input is null, replace with empty string ""
+            string input = Console.ReadLine() ?? "";
+            if (input == "")
+            {
+                break;
+            }
+            employees.Add(input);
+        }
+        return employees;
+    }
+    static void PrintEmplayees(List<string> employees)
+    {
         for (int i = 0; i < employees.Count; i++)
         {
             Console.WriteLine(employees[i]);
         }
+    }
+    static void Main(string[] args)
+    {
+        List<string> employees = GetEmployees();
+        PrintEmplayees(employees);
     }
   }
 }
