918436d291061ace5cb07e7562404ec8f00687ac
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-08-29 21:53:44 -0500

    foreach loop parses json data to create a badge for each employee from API call

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -35,11 +35,25 @@ namespace  CatWorx.BadgeMaker
 
         async public static Task<List<Employee>> GetFromApi()
         {
+            List<Employee> employees = new List<Employee>();
+
             using (HttpClient client = new HttpClient())
             {
                 string response = await client.GetStringAsync("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
+                JObject json = JObject.Parse(response);
+                
+                foreach (JToken token in json.SelectToken("results")!) {
+                    Employee emp = new Employee
+                    (
+                        token.SelectToken("name.first")!.ToString(),
+                        token.SelectToken("name.last")!.ToString(),
+                        Int32.Parse(token.SelectToken("id.value")!.ToString().Replace("-", "")),
+                        token.SelectToken("picture.large")!.ToString()
+                    );
+                    employees.Add(emp);
+                }
             }
-            List<Employee> employees = new List<Employee>();
+            
             return employees;
         }
     }
